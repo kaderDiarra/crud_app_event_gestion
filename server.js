62fc2e8741d14cb5d149c0e2c922805d01f5f745
Kader Diarra <kader@linux.home>
2020-07-26 19:20:35 +0200

    [ADD => SETUP WEBSERVER] Configuration of cors

--- server.js
+++ server.js
@@ -2,3 +2,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const app = express();
+
+// Configuration du CORS
+var corsOptions = {
+    origin: "http://localhost:8081",
+    optionsSucessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
