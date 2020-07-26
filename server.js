bb729e9e44bfe533d4c29c7333afe7156c8f49c4
Kader Diarra <kader@linux.home>
2020-07-26 19:59:03 +0200

    [ADD => WEB SERVER] Configuration of body-parser middleware | creation of simple route | launching and connexion of server on environment variable PORT or 3000

--- server.js
+++ server.js
@@ -3,13 +3,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+// PORT
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
-// Configuration du CORS
+// cors middleware config
 var corsOptions = {
-    origin: "http://localhost:8081",
+    origin: `http://localhost:${PORT}`,
     optionsSucessStatus: 200
 };
 
 app.use(cors(corsOptions));
 
+// parse incoming request with body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// route racine
+app.get("/", (req, res) => {
+    res.json({message: "Itrut CRUD events gestion"});
+});
+
+// Launch server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
