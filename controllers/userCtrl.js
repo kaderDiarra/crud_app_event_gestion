153018906d26aed84d4d13d38c6023ba5812988e
Kader Diarra <kader@linux.home>
2020-07-27 14:29:08 +0200

    [ADD => WEB SERVER => USER CONTROLLER] check if user already exist or no

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,11 +1,35 @@
 // Import
 var bcrypt = require("bcrypt");
 var jwt = require("jsonwebtoken");
+var models = require("../models");
 
 // Routes
 module.exports = {
     register: (req, res) => {
-        // TODO: To implement
+        // Parameters
+        var username = req.body.username;
+        var email = req.body.email;
+        var password = req.body.password;
+
+        if (username == null || email == null || password == null) {
+            return res.status(400).json({"error": "Il manque des paramètres"});
+        }
+
+        // TODO verify pseudo length, mail, password ...
+        models.User.findOne({
+            attributes: ['email'],
+            where: {email: email}
+        })
+        .then((userFound) => {
+            if (!userFound) {
+                // create user
+            } else {
+                return res.status(409).json({"error": "Compte déjà existant"});
+            }
+        })
+        .catch((err) => {
+            return res.status(500).json({"error": "Authentification impossible"});
+        });
     },
     login: (req, res) => {
         // TODO: To implement
